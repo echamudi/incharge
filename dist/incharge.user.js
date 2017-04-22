e71db126cd25abc95c85cc019ce1d1c602407613
Ezzat Chamudi <ezhmd@users.noreply.github.com>
2017-04-22 18:34:13 +0300

    Added files

---
+++ dist/incharge.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Incharge
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Recharge InVision App with additional features
+// @author       https://github.com/ezhmd
+// @match        https://projects.invisionapp.com/
+// @grant        none
+// ==/UserScript==
+(function () {
+    'use strict';
+    // Your code here...
+})();
---
+++ incharge.user.ts
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Incharge
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Recharge InVision App with additional features
+// @author       https://github.com/ezhmd
+// @match        https://projects.invisionapp.com/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
