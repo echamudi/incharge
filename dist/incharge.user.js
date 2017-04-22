65af70120063ec9b20ae85f0cfdb537039c9b5cd
Ezzat Chamudi <ezhmd@users.noreply.github.com>
2017-04-22 18:42:08 +0300

    merge

--- dist/incharge.user.js
+++ dist/incharge.user.js
@@ -1,7 +1,11 @@
 // ==UserScript==
 // @name         Incharge
 // @namespace    http://tampermonkey.net/
+<<<<<<< HEAD:incharge.user.js
 // @version      0.4
+=======
+// @version      0.3
+>>>>>>> a44d86b... Update:dist/incharge.user.js
 // @description  Recharge InVision App with additional features
 // @author       https://github.com/ezhmd
 // @match        https://projects.invisionapp.com/*
